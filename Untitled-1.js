2ceadcfb26e9bcfa79d6f5d6f5bc4cb2f473829c
palchikmrg <78450570+palchikmrg@users.noreply.github.com>
2022-06-15 15:29:27 +0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,151 @@
+
+number 3
+
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
+
+
+
+// 4 - Assigning the Value of One Variable to Another
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a
+
+
+// 5 - Initializing Variables with the Assignment Operator
+
+var a = 9;
+
+
+// 6 - Declare String Variables
+
+var myFirstName = "Qwe"
+var myLastName = "Qaz"
+
+
+// 7 - Understanding Uninitialized Variables
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// 8 - Understanding Case Sensitivity in Variables
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver= 9000;
+
+
+
+// 9 - Explore Differences Between the var and let Keywords
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
+// 10 - Declare a Read-Only Variable with the const Keyword
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+// 11 - Add Two Numbers with JavaScript
+
+const sum = 10 + 10;
+
+
+// 12 - Subtract One Number from Another with JavaScript
+
+const difference = 45 - 33;
+
+
+// 13 - Multiply Two Numbers with JavaScript
+
+const product = 8 * 10;
+
+
+// 14 - Divide One Number by Another with JavaScript
+
+const quotient = 66 / 33;
+
+
+// 15 Increment a Number with JavaScript
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+
+// 16 Decrement a Number with JavaScript
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+
+// 17 Create Decimal Numbers with JavaScript
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
+
+
+// 18 Multiply Two Decimals with JavaScript
+
+const product = 2.0 * 2.5;
+
+
+// 19 Divide One Decimal by Another with JavaScript
+
+const quotient = 4.4 / 2.0; // Change this line
+
+
+// 20 Finding a Remainder in JavaScript
+
+const remainder = 11 % 3;
+
+
+// 21 Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+// 22 Compound Assignment With Augmented Subtraction
+
+
+
+
+
