535fd8646a58d03ad9695dff1d610ebcefb2c03e
palchikmrg <palchik.mrg@gmail.com>
2022-06-16 00:11:19 +0300

    commit - 1

--- basicJS.js
+++ basicJS.js
@@ -144,7 +144,57 @@ c += 7;
 
 
 // 22 Compound Assignment With Augmented Subtraction
-bjjb
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+// 23 Compound Assignment With Augmented Multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+// 24 Compound Assignment With Augmented Division
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+// 25 Escaping Literal Quotes in Strings
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
+// 26 Quoting Strings with Single Quotes
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+// 27 Escape Sequences in Strings
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+// 28 
 
 
 
