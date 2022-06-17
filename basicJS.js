41b5c2116ae56ebe0999da928a8ae639fe37dbde
palchikmrg <palchik.mrg@gmail.com>
2022-06-17 06:40:35 +0300

    commit 54-71

--- basicJS.js
+++ basicJS.js
@@ -438,6 +438,259 @@ function myLocalScope() {
 
 //  54 Global vs. Local Scope in Functions
 
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+
+// 55 - Understanding Undefined Value returned from a Function
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code above this line
+
+addThree();
+addFive();
+
+
+// 56 Assignment with a Returned Value
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+ processed = processArg(7);
+
+
+//  57 Stand in Line
+
+
+// 58 Understanding Boolean Values
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+
+// 59 Use Conditional Logic with If Statements
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+
+  if (wasThatTrue) {
+    return "Yes, that was true";
+ }
+ return "No, that was false";
+  // Only change code above this line
+
+}
+
+
+// 60 Comparison with the Equality Operator
+
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+
+// 61 Comparison with the Strict Equality Operator
+
+// Setup
+function testStrict(val) {
+  if (val ===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
+
+
+// 62 Practice comparing different values
+
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+
+// 63 Comparison with the Inequality Operator
+
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+
+// 64 Comparison with the Strict Inequality Operator
+
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+
+
+// 65 Comparison with the Greater Than Operator
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+
+// 66 Comparison with the Greater Than Or Equal To Operator
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+
+// 67 Comparison with the Less Than Operator
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+
+// 68 Comparison with the Less Than Or Equal To Operator
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+
+// 69 Comparisons with the Logical And Operator
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+
+// 70 Comparisons with the Logical Or Operator
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+
+// 71 Introducing Else Statements
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller"
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+
+// 72 
+
 
 
 
